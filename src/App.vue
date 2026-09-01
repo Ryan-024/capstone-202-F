5b643cc06a2026563a69a92a3e5256f922736801
Ryan.Dampf <ryan.dampf@slalom.com>
2026-09-01 17:56:34 -0500

    build 1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" density="compact">
+      <v-app-bar-title>
+        <v-icon icon="mdi-view-dashboard" class="mr-2" />
+        my-dashboard
+      </v-app-bar-title>
+      <v-spacer />
+      <v-btn :to="{ name: 'home' }" prepend-icon="mdi-home">Home</v-btn>
+      <v-btn :to="{ name: 'about' }" prepend-icon="mdi-information">About</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
